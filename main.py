648c94169ee646a569c1dfd23c28821892007a97
agirmai <agirmai737@gmail.com>
2026-02-07 12:53:40 -0600

    Got playwright working with a basic website

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 print("Hello World")
 print("Testing testing")
+
+from playwright.sync_api import sync_playwright, Playwright
+
+def run(playwright: Playwright):
+    chrome = playwright.chromium
+    browser = chrome.launch()
+    page = browser.new_page()
+    page.goto("https://abemgirmai.pages.dev/")
+    answer = page.locator("h1").first
+    print(answer.text_content())
+
+with sync_playwright() as playwright:
+    run(playwright)
+
+
+
